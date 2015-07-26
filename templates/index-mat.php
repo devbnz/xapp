f02dcd709034864d0636f257cdb170c9fba9b1e1
Erik Woitschig <erik@sourcloud.com>
2015-07-26 22:57:33 +0200

    layout changes

--- templates/index-mat.php
+++ templates/index-mat.php
@@ -21,11 +21,6 @@
     <meta name="msapplication-TileImage" content="images/touch/ms-touch-icon-144x144-precomposed.png">
     <meta name="msapplication-TileColor" content="#3372DF">
 
-    <!-- SEO: If your mobile URL is different from the desktop URL, add a canonical link to the desktop page https://developers.google.com/webmasters/smartphone-sites/feature-phones -->
-    <!--
-    <link rel="canonical" href="http://www.example.com/">
-    -->
-
     <link href="https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=en" rel="stylesheet">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/material.min.css">
@@ -81,46 +76,14 @@
     <div class="demo-blog mdl-layout mdl-js-layout has-drawer is-upgraded">
       <main class="mdl-layout__content">
         <div class="demo-blog__posts mdl-grid">
-          <!--
-          <div class="mdl-card coffee-pic mdl-cell mdl-cell--8-col">
-            <div class="mdl-card__media mdl-color-text--grey-50">
-              <h3><a href="entry.html">Hello <?= $displayName ?></a></h3>
-            </div>
-            <div class="mdl-card__supporting-text meta mdl-color-text--grey-600">
-
-              <div>
-
-              </div>
-            </div>
-          </div>
-        -->
           <div class="mdl-card something-else mdl-cell mdl-cell--8-col mdl-cell--4-col-desktop">
-          <!--
-            <button class="mdl-button mdl-js-ripple-effect mdl-js-button mdl-button--fab mdl-color--accent">
-              <i class="material-icons mdl-color-text--white" role="presentation">add</i>
-              <span class="visuallyhidden">add</span>
-            </button>
-          -->
             <div class="mdl-card__media mdl-color--white mdl-color-text--grey-600">
               <img src="<?= $photoURL ?>">
-            <!--  +1,337-->
             </div>
             <div class="mdl-card__supporting-text meta meta--fill mdl-color-text--grey-600">
               <div>
                 <strong><?= $displayName ?></strong>
               </div>
-              <!--
-              <ul class="mdl-menu mdl-js-menu mdl-menu--bottom-right" for="menubtn">
-                <li class="mdl-menu__item mdl-js-ripple-effect">About</li>
-                <li class="mdl-menu__item mdl-js-ripple-effect">Message</li>
-                <li class="mdl-menu__item mdl-js-ripple-effect">Favorite</li>
-                <li class="mdl-menu__item mdl-js-ripple-effect">Search</li>
-              </ul>
-              <button id="menubtn" class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon">
-                <i class="material-icons" role="presentation">more_vert</i>
-                <span class="visuallyhidden">show menu</span>
-              </button>
-            -->
             </div>
           </div>
 
@@ -128,8 +91,8 @@
               <div class="mdl-color-text--grey-600 mdl-card__supporting-text">
             <? if ($isLoggedIn) { ?>
               <a href="logout"><button class="mdl-button mdl-js-button mdl-button--raised">LOGOUT</button></a>
-              <a id="contacts"><button id="contacts" class="mdl-button mdl-js-button mdl-button--raised"><i class="material-icons">group</i> Contacts</button></a>
-              <button href="find/appmedia" class="mdl-button mdl-js-button mdl-button--raised"><i class="material-icons">group</i> Search (ie appmedia)</button>
+              <a id="contacts"><button id="contacts" class="mdl-button mdl-js-button mdl-button--raised">Contacts</button></a>
+              <button class="mdl-button mdl-js-button mdl-button--raised">Search</button>
             <? } else { ?>
               <a href="login"><button class="mdl-button mdl-js-button mdl-button--raised">LOGIN via XING</button></a>
             <? } ?>
@@ -142,18 +105,6 @@
               <h3 class="quote">Many people go differnt ways to see the same thing.</a></h3>
               </div>
             </div>
-            <!--
-            <div class="mdl-card__supporting-text mdl-color-text--grey-600">
-              Enim labore aliqua consequat ut quis ad occaecat aliquip incididunt. Sunt nulla eu enim irure enim nostrud aliqua consectetur ad consectetur sunt ullamco officia. Ex officia laborum et consequat duis.
-            </div>
-            <div class="mdl-card__supporting-text meta mdl-color-text--grey-600">
-              <div class="minilogo"></div>
-              <div>
-                <strong>The Newist</strong>
-                <span>2 days ago</span>
-              </div>
-            </div>
-          -->
           </div>
 
           <div class="mdl-card on-the-road-again mdl-cell mdl-cell--12-col">
@@ -172,19 +123,6 @@
 
 
         </div>
-        <!--
-          <footer class="mdl-mini-footer">
-          <div class="mdl-mini-footer--left-section">
-
-          </div>
-          <div class="mdl-mini-footer--right-section">
-            <button class="mdl-mini-footer--social-btn social-btn__share">
-              <i class="material-icons" role="presentation">share</i>
-              <span class="visuallyhidden">share</span>
-            </button>
-          </div>
-        </footer>
-      -->
       </main>
       <div class="mdl-layout__obfuscator"></div>
     </div>
