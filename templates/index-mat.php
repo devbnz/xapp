f4b98ed6821692c1f3fda4b2f83418ce73e186f5
Erik Woitschig <erik@sourcloud.com>
2015-07-26 18:53:31 +0200

    layout changes

--- templates/index-mat.php
+++ templates/index-mat.php
@@ -81,23 +81,19 @@
     <div class="demo-blog mdl-layout mdl-js-layout has-drawer is-upgraded">
       <main class="mdl-layout__content">
         <div class="demo-blog__posts mdl-grid">
+          <!--
           <div class="mdl-card coffee-pic mdl-cell mdl-cell--8-col">
             <div class="mdl-card__media mdl-color-text--grey-50">
               <h3><a href="entry.html">Hello <?= $displayName ?></a></h3>
             </div>
             <div class="mdl-card__supporting-text meta mdl-color-text--grey-600">
-              <div class="minilogo"></div>
+
               <div>
-                <? if ($isLoggedIn) { ?>
-                  <a href="logout" class="btn btn-danger">Logout</a>
-                  <a id="contacts" class="btn btn-danger">Contacts</a>
-                  <a href="find/appmedia" class="btn btn-danger">Search (ie appmedia)</a>
-                <? } else { ?>
-                  <a href="login" class="btn btn-primary">Login</a>
-                <? } ?>
+
               </div>
             </div>
           </div>
+        -->
           <div class="mdl-card something-else mdl-cell mdl-cell--8-col mdl-cell--4-col-desktop">
           <!--
             <button class="mdl-button mdl-js-ripple-effect mdl-js-button mdl-button--fab mdl-color--accent">
@@ -128,6 +124,17 @@
             </div>
           </div>
 
+          <div class="mdl-card on-the-road-again mdl-cell mdl-cell--12-col">
+              <div class="mdl-color-text--grey-600 mdl-card__supporting-text">
+            <? if ($isLoggedIn) { ?>
+              <a href="logout"><button class="mdl-button mdl-js-button mdl-button--raised">LOGOUT</button></a>
+              <a id="contacts"><button id="contacts" class="mdl-button mdl-js-button mdl-button--raised"><i class="material-icons">group</i> Contacts</button></a>
+              <button href="find/appmedia" class="mdl-button mdl-js-button mdl-button--raised"><i class="material-icons">group</i> Search (ie appmedia)</button>
+            <? } else { ?>
+              <a href="login"><button class="mdl-button mdl-js-button mdl-button--raised">LOGIN via XING</button></a>
+            <? } ?>
+          </div>
+          </div>
 
           <div class="mdl-card amazing mdl-cell mdl-cell--12-col">
             <div class="mdl-card__title mdl-color-text--grey-50">
@@ -135,6 +142,7 @@
               <h3 class="quote">Many people go differnt ways to see the same thing.</a></h3>
               </div>
             </div>
+            <!--
             <div class="mdl-card__supporting-text mdl-color-text--grey-600">
               Enim labore aliqua consequat ut quis ad occaecat aliquip incididunt. Sunt nulla eu enim irure enim nostrud aliqua consectetur ad consectetur sunt ullamco officia. Ex officia laborum et consequat duis.
             </div>
@@ -145,36 +153,29 @@
                 <span>2 days ago</span>
               </div>
             </div>
+          -->
           </div>
-
 
           <div class="mdl-card on-the-road-again mdl-cell mdl-cell--12-col">
-            <div class="mdl-card__media mdl-color-text--grey-50">
-              <h3><a href="entry.html">On the road again</a></h3>
-            </div>
-            <div class="mdl-color-text--grey-600 mdl-card__supporting-text">
-              <p>This application is part of the <b>platform</b> project by Erik Woitschig.<br/>
-              http://bnz-power.com</p>  </div>
-            <div class="mdl-card__supporting-text meta mdl-color-text--grey-600">
-              <div class="minilogo"></div>
-              <div>
-                <strong>The Newist</strong>
-                <span>2 days ago</span>
+              <div class="mdl-color-text--grey-600 mdl-card__supporting-text">
+                <button class="mdl-mini-footer--social-btn social-btn social-btn__twitter">
+                  <span class="visuallyhidden">Twitter</span>
+                </button>
+                <button class="mdl-mini-footer--social-btn social-btn social-btn__blogger">
+                  <span class="visuallyhidden">Facebook</span>
+                </button>
+                <button class="mdl-mini-footer--social-btn social-btn social-btn__gplus">
+                  <span class="visuallyhidden">Google Plus</span>
+                </button>
               </div>
-            </div>
           </div>
+
+
         </div>
+        <!--
           <footer class="mdl-mini-footer">
           <div class="mdl-mini-footer--left-section">
-            <button class="mdl-mini-footer--social-btn social-btn social-btn__twitter">
-              <span class="visuallyhidden">Twitter</span>
-            </button>
-            <button class="mdl-mini-footer--social-btn social-btn social-btn__blogger">
-              <span class="visuallyhidden">Facebook</span>
-            </button>
-            <button class="mdl-mini-footer--social-btn social-btn social-btn__gplus">
-              <span class="visuallyhidden">Google Plus</span>
-            </button>
+
           </div>
           <div class="mdl-mini-footer--right-section">
             <button class="mdl-mini-footer--social-btn social-btn__share">
@@ -183,6 +184,7 @@
             </button>
           </div>
         </footer>
+      -->
       </main>
       <div class="mdl-layout__obfuscator"></div>
     </div>
